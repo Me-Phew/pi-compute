c7ec432aeb192a8d278bf07df7daee2feb8e16f7
Me-Phew <basmateusz@wp.pl>
2024-12-19 01:02:58 +0100

    feat: add driver code in main function

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,20 @@
  */
 
 #include <iostream>
-
+#include "integral_calculator/integral_calculator.hpp"
 
 int main() {
+  long long intervals;
+  std::cout << "Podaj liczbe przedzialow: ";
+  std::cin >> intervals;
 
+  long long threads;
+  std::cout << "Podaj liczbe watkow: ";
+  std::cin >> threads;
+
+  IntegralCalculator integralCalculator(intervals, threads);
+  integralCalculator.calculate();
+
+  std::cout << "PI ~=" << integralCalculator.get_result() << std::endl;
+  std::cout << "Czas obliczen: " << integralCalculator.get_computation_time() << " sekund" << std::endl;
 }
